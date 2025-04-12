4881f1f7ae2c73ec7ea9efeb3c0fd342992ba505
vyn <jhovynn3@gmail.com>
2025-04-13 05:10:03 +0800

    added a devSourcemap code for frontend devs

--- Frontend_Capstone1/capstoneconnect/vite.config.js
+++ Frontend_Capstone1/capstoneconnect/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    devSourcemap: true, // hehe for source map debugging for frontend editing <33
+  }
 })
